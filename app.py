4af0403b93f35c5c7bf0fd9515dd2df094f70bdc
sankarmr <67607865+sankarmr@users.noreply.github.com>
2023-06-22 20:10:02 +0000

    Created job list array

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Dubai',
+  'salary': 'Dhs 100,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Trivandrum',
+  'salary': 'Rs. 150,000'
+}, {
+  'id': 3,
+  'title': 'Soft Engineer',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 110,000'
+}, {
+  'id': 4,
+  'title': 'Graphics Designer',
+  'location': 'New York, United States',
+  'salary': '$90,000'
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Uthra's")
 
 
 if __name__ == "__main__":
